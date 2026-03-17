c9f1108d9829ae267d2153d61d20cc0153b06b0d
Victor Chicas <victorchicas525@gmail.com>
2026-03-17 03:10:50 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -10,4 +10,5 @@ const SUPABASE_URL = 'https://uaaecubrpbkshtgozefb.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYWVjdWJycGJrc2h0Z296ZWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MTcyODAsImV4cCI6MjA4OTI5MzI4MH0.c7mGFniboCKJ_pHkeV-0qeiULityMsABH5-w4ubMriI';
 
 // Inicializar Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const { createClient } = window.supabase;
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
