d8b016086fa6f64b107f755c4e9bb3331d2e7b06
Victor Chicas <victorchicas525@gmail.com>
2026-03-17 03:14:03 -0300

    Teste

--- supabase-config.js
+++ supabase-config.js
@@ -1,14 +1,12 @@
-// ========================================
-// CONFIGURAÇÃO DO SUPABASE - IMPERIO
-// ========================================
-//
-// Siga o tutorial para obter essas informações
-//
-// ========================================
+  // ========================================
+  // CONFIGURAÇÃO DO SUPABASE - IMPERIO
+  // ========================================
 
-const SUPABASE_URL = 'https://uaaecubrpbkshtgozefb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYWVjdWJycGJrc2h0Z296ZWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MTcyODAsImV4cCI6MjA4OTI5MzI4MH0.c7mGFniboCKJ_pHkeV-0qeiULityMsABH5-w4ubMriI';
+  const SUPABASE_URL = 'https://uaaecubrpbkshtgozefb.supabase.co';
+  const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYWVjdWJycGJrc2h0Z296ZWZiIiwicm9sZSI6ImFub24i
+  LCJpYXQiOjE3NzM3MTcyODAsImV4cCI6MjA4OTI5MzI4MH0.c7mGFniboCKJ_pHkeV-0qeiULityMsABH5-w4ubMriI';
 
-// Inicializar Supabase
-const { createClient } = window.supabase;
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  // Inicializar Supabase
+  const { createClient } = window.supabase;
+  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
