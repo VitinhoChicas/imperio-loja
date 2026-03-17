96a40941181630cec2424e4046d3deb349edd093
Victor Chicas <victorchicas525@gmail.com>
2026-03-17 03:05:12 -0300

    Alteração da chave

--- supabase-config.js
+++ supabase-config.js
@@ -7,7 +7,7 @@
 // ========================================
 
 const SUPABASE_URL = 'https://uaaecubrpbkshtgozefb.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_8xY63ytLFgJiLiRXkhNMNA_G4D1uUM6';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYWVjdWJycGJrc2h0Z296ZWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MTcyODAsImV4cCI6MjA4OTI5MzI4MH0.c7mGFniboCKJ_pHkeV-0qeiULityMsABH5-w4ubMriI';
 
 // Inicializar Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
