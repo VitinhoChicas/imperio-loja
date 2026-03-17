e9124f2c36ec216dc796484a0b33b4b304921066
Victor Chicas <victorchicas525@gmail.com>
2026-03-17 03:16:06 -0300

    Teste chave

--- supabase-config.js
+++ supabase-config.js
@@ -4,8 +4,7 @@
 
   const SUPABASE_URL = 'https://uaaecubrpbkshtgozefb.supabase.co';
   const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYWVjdWJycGJrc2h0Z296ZWZiIiwicm9sZSI6ImFub24i
-  LCJpYXQiOjE3NzM3MTcyODAsImV4cCI6MjA4OTI5MzI4MH0.c7mGFniboCKJ_pHkeV-0qeiULityMsABH5-w4ubMriI';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhYWVjdWJycGJrc2h0Z296ZWZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3MTcyODAsImV4cCI6MjA4OTI5MzI4MH0.c7mGFniboCKJ_pHkeV-0qeiULityMsABH5-w4ubMriI';
 
   // Inicializar Supabase
   const { createClient } = window.supabase;
